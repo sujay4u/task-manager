5e41aab20cb8d367ed5e99cb855091e70641aee4
sujay4u <sujay.hadadare@gmail.com>
2025-08-17 16:05:28 +0530

    feature : Added task update logic

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -12,3 +12,19 @@ exports.createTask = (req,res) =>{
         res.status(201).json({message:'Task created', taskId: result.insertId});
     });
 };
+
+//Update
+exports.updateTask =(req, res) =>
+{   
+    const {id} = req.params;
+    const sql = 'UPDATE tasks SET title = ?, description = ?, status = ? WHERE id = ?';
+    const {title,description,status} = req.body;
+
+    db.query(sql, [title, description, status, id] ,(err, result)=>
+    {
+        if(err) return res.status(500).json({error:err.message});
+        if(result.affectedRows === 0) return res.status(404).json({error:'Task not found'});
+
+        res.status(200).json({message:'Task Updated', updatedId: id })
+    })
+}
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
 
-const { createTask } = require('../controllers/taskController');
+const { createTask, updateTask } = require('../controllers/taskController');
 
 //Routes
-router.post('/', createTask);
+router.post('/', createTask); //create task
+
+
+router.put('/:id', updateTask); //update task
 
 module.exports = router;
