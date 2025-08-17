c367d4019f0403b084e9475a60dae7e205ca828e
sujay4u <sujay.hadadare@gmail.com>
2025-08-17 16:15:22 +0530

    feature :api for displaying all task or specific task

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -42,3 +42,33 @@ exports.deleteTask =(req, res)=>{
         res.json({message:'Task Deleted',deleteId:id});
     });
 }
+
+//Select
+exports.getAllTasks = (req,res)=>{
+    const sql = 'SELECT * FROM tasks ORDER BY created_at DESC';
+    db.query(sql,(err,result)=>{
+        if(err) return res.status(500).json({
+            error:err.message
+        });
+        res.json(result);
+    })
+}
+
+//Select by ID
+exports.getTaskById = (req, res) => {
+    const { id } = req.params;
+
+    const sql = 'SELECT * FROM tasks WHERE id = ?';
+    db.query(sql, [id], (err, results) => {
+        if (err) {
+            console.error('Error fetching task:', err);
+            return res.status(500).json({ error: 'Database error' });
+        }
+
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'Task not found' });
+        }
+
+        res.status(200).json(results[0]);
+    });
+};
--- routes/taskRoutes.js
+++ routes/taskRoutes.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const router = express.Router();
 
-const { createTask, updateTask, deleteTask } = require('../controllers/taskController');
+const { createTask, updateTask, deleteTask, getTaskById, getAllTasks } = require('../controllers/taskController');
 
 //Routes
 router.post('/', createTask); //create task
+router.get('/',getAllTasks) //show all tasks
 
 
 router.put('/:id', updateTask); //update task
 router.delete('/:id', deleteTask) //delete task
+router.get('/:id',getTaskById) //show specific task
 
 module.exports = router;
